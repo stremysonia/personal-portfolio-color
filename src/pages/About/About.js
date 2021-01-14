07c4a80f0097facbfaffe692f5db62f974a3b22b
Sonia <st.remysonia@gmail.com>
2021-01-13 22:36:17 -0500

    centered and aligned the words that I used to desribe myself in the about page

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -3,31 +3,26 @@ import './About.css';
 
 const About = () => {
         return (
-            <>
+        <>
             <div className="about-container">
                 <div className="about-paragraph">
-                    <p className="bio"> My name is Sonia St. Remy. I was born and raised in the Miami,
-                                Florida area where the mix of various cultures has made me the person I
-                                am today. My career has been spent working two years in the social
-                                service in drug treatment and ten years in various customer service
-                                roles. I enjoyed my time in these career fields but I realized I wanted
-                                to help more people on a larger scale. So, I decided to make a career
-                                change and attended Wyncode Academy where I graduated with a certificate
-                                in Full Stack Web Development. After graduating I have been sharpening
-                                my skills to be the best Front End Web Developer I can be. I have
-                                finally found the career that will blend my passion for creativity and my
-                                love for technology.
+                    <p className="bio"> 
+                        My name is Sonia St. Remy. I was born and raised in the Miami,
+                        Florida area where the mix of various cultures has made me the person I
+                        am today. My career has been spent working two years in the social
+                        service in drug treatment and ten years in various customer service
+                        roles. I enjoyed my time in these career fields but I realized I wanted
+                        to help more people on a larger scale. So, I decided to make a career
+                        change and attended Wyncode Academy where I graduated with a certificate
+                        in Full Stack Web Development. After graduating I have been sharpening
+                        my skills to be the best Front End Web Developer I can be. I have
+                        finally found the career that will blend my passion for creativity and my
+                        love for technology.
+                        <div className="words">Edgy | Creative | Passionate | Inquistive | Charismatic</div>
                     </p>
-                    </div>
-                        <div className="about-description">
-                            <div className="words">Edgy</div>
-                            <div className="words">Creative</div>
-                            <div className="words">Passionate</div>
-                            <div className="words">Inquistive</div>
-                            <div className="words">Charismatic</div>
-                        </div>
+                </div>
             </div>
-            </>
+        </>
         );
 }
 export default About;
